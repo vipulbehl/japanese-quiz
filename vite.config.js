6b3efec3af2b271662e4a3498e7b08932df5431b
Vipul Behl <vipulbehl20@gmail.com>
2023-12-31 13:17:29 +0530

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://mayank-behl.github.io/japanese-quiz/",
+  base: "https://vipulbehl.github.io/japanese-quiz/",
   plugins: [react()],
 });
